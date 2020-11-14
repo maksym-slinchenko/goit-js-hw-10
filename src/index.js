52138b253ca9849fe736a89644cbc98cd1afe476
Maksym Slinchenko <max.slin777@gmail.com>
2020-11-14 08:37:09 +0200

    optimization of functions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './styles.css';
 import menuAr from './menu.json';
 import cardCreater from './templates/menu-meal.hbs';
 
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
 
 const menuEl = document.querySelector('.js-menu');
 const themeSwitcherEl = document.querySelector('.theme-switch__toggle');
@@ -13,32 +14,30 @@ const bodyEl = document.querySelector('body');
 
 menuEl.insertAdjacentHTML('beforeend', cardCreater(menuAr));
 // -----------
-const addLightTheme = () => {
-  bodyEl.classList.add('light-theme');
-  bodyEl.classList.remove('dark-theme');
+const addTheme = (addedTheme, removedTheme) => {
+  bodyEl.classList.add(addedTheme);
+  bodyEl.classList.remove(removedTheme);
 };
 
-const addDarkTheme = () => {
-  bodyEl.classList.add('dark-theme');
-  bodyEl.classList.remove('light-theme');
+const setThemeToLocalSt = (themeObject, indexSetTheme, indexRemovedTheme) => {
+  localStorage.setItem(
+    Object.keys(themeObject)[indexSetTheme],
+    Object.values(themeObject)[indexSetTheme],
+  );
+  localStorage.removeItem(Object.keys(themeObject)[indexRemovedTheme]);
 };
 
-const setLightToLocalSt = () => {
-  localStorage.setItem('LIGHT', 'light-theme');
-  localStorage.removeItem('DARK');
-};
-
-const setDarkToLocalSt = () => {
-  localStorage.setItem('DARK', 'dark-theme');
-  localStorage.removeItem('LIGHT');
-};
+// const setDarkToLocalSt = () => {
+//   localStorage.setItem('DARK', 'dark-theme');
+//   localStorage.removeItem('LIGHT');
+// };
 // -------------
 const setStartTheme = () => {
-  if (Object.keys(localStorage).includes('DARK')) {
-    addDarkTheme();
+  if (Object.keys(localStorage).includes(Object.keys(Theme)[1])) {
+    addTheme(DARK, LIGHT);
     themeSwitcherEl.checked = true;
   } else {
-    addLightTheme();
+    addTheme(LIGHT, DARK);
     themeSwitcherEl.checked = false;
   }
 };
@@ -46,13 +45,12 @@ setStartTheme(bodyEl);
 
 const changingTheme = () => {
   if (themeSwitcherEl.checked === false) {
-    addLightTheme();
-    setLightToLocalSt();
+    addTheme(LIGHT, DARK);
+    setThemeToLocalSt(Theme, 0, 1);
   } else {
-    addDarkTheme();
-    setDarkToLocalSt();
+    addTheme(DARK, LIGHT);
+    setThemeToLocalSt(Theme, 1, 0);
   }
 };
 
 themeSwitcherEl.addEventListener('change', changingTheme);
-console.log(test);
